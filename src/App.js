b7ed7bbe7bff51ee67c11798ca16f6792a15d36e
ganesh asil <ganeshasil464@gmail.com>
2024-01-09 07:47:20 +0530

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-// import FormComponent from "./Components/Form/form";
-// import ListCompenent from "./Components/List/list";
-// import TableCompenent from "./Components/Table/table";
+import FormComponent from "./Components/Form/form";
+import ListCompenent from "./Components/List/list";
+import TableCompenent from "./Components/Table/table";
 
 
-import Hello,{Hello1 as Hel1,Hello2 as Hel2} from "./NamedExport/Namedexport";
+// import Hello,{Hello1 as Hel1,Hello2 as Hel2} from "./NamedExport/Namedexport";
 
 
 function App() {
 
+//   return(
+//     <Hello/>
+//   )
+
   return(
+
+    <div>
     <Hello/>
+    <Hel1/>
+    <Hel2/>
+    </div>
+
   )
 
-  // return(
-
-  //   <div>
-  //   <Hello/>
-  //   <Hel1/>
-  //   <Hel2/>
-  //   </div>
-
-  // )
 
 
-
-  // return (
-  //   <div>
-  //     <FormComponent/>
-  //     <ListCompenent/>
-  //     <TableCompenent/>
+  return (
+    <div>
+      <FormComponent/>
+      <ListCompenent/>
+      <TableCompenent/>
     
-  //   </div>
+    </div>
 
 
 
-  // )
+  )
 
  
 }
