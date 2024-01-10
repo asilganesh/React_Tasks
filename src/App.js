8bf0dabc1a488a531390fbb5417944d8253bfdd7
ganesh asil <ganeshasil464@gmail.com>
2024-01-10 16:50:31 +0530

    added tasks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import FormComponent from "./Components/Form/form";
-import ListCompenent from "./Components/List/list";
-import TableCompenent from "./Components/Table/table";
+// import FormComponent from "./Components/Form/form";
+// import ListCompenent from "./Components/List/list";
+// import TableCompenent from "./Components/Table/table";
+
+import DarkandNight from "./Components/09Dec/DarkandNight";
 
 
 // import Hello,{Hello1 as Hel1,Hello2 as Hel2} from "./NamedExport/Namedexport";
 
 
+
 function App() {
+
+
+return(
+  <DarkandNight/>
+)
+
 
 //   return(
 //     <Hello/>
 //   )
 
-  return(
+  // return(
 
-    <div>
-    <Hello/>
-    <Hel1/>
-    <Hel2/>
-    </div>
+  //   <div>
+  //   <Hello/>
+  //   <Hel1/>
+  //   <Hel2/>
+  //   </div>
 
-  )
+  // )
 
 
 
-  return (
-    <div>
-      <FormComponent/>
-      <ListCompenent/>
-      <TableCompenent/>
+  // return (
+  //   <div>
+  //     <FormComponent/>
+  //     <ListCompenent/>
+  //     <TableCompenent/>
     
-    </div>
+  //   </div>
 
 
 
-  )
+  // )
 
  
 }
---
+++ src/Components/09Dec/DarkandNight.js
@@ -0,0 +1,68 @@
+import { Component } from "react";
+
+
+
+class DarkandNight extends Component{
+
+    state={
+        colorone:"gray",
+        colortwo:"black",
+        clicked:true,
+        themeone:"Theme is white",
+        themetwo:"Theme is Dark"
+    }
+
+     handleEvent=()=>{
+        this.setState(
+            {
+                clicked:!this.state.clicked
+            },()=>{
+                console.log(this.state.clicked)
+            }
+        )
+       
+    }
+
+    render(){
+        return(
+            
+            <>
+            <div style={{width:"100px",height:"100px",border:"1px solid transparent",margin:"auto auto",
+            backgroundColor:this.state.clicked?this.state.colorone:this.state.colortwo}} 
+            onClick={this.handleEvent}>
+
+            </div>
+
+            <p style={{textAlign:"center"}}>
+                {
+                    this.state.clicked?this.state.themeone:this.state.themetwo
+                }
+         
+            </p>
+            </>
+
+        )
+    }
+
+// state={
+//     message:"clickme",
+//     messagestatus:true,
+// }
+//  ClickEvent=()=>{
+//     this.setState({
+//         messagestatus:!this.state.messagestatus
+//     },()=>{
+//         console.log(this.state.messagestatus)
+//     })
+// }
+
+// render(){
+
+//     return(
+//         <button onClick={this.ClickEvent}>{this.state.message}</button>
+//     )
+// }
+
+}
+
+export default DarkandNight
